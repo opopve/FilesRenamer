2242390454aee3bccd83d1222924975d0778f5d2
OO <opopve@gmail.com>
2019-12-04 22:06:52 +0300

    InitialCommit

---
+++ FilesRenamer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FilesRenamer
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join('\n', args));
+            var key = Console.ReadKey();
+            if (key.KeyChar == 'e')
+                return;
+        }
+    }
+}
