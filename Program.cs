2747e0086d04de6b0fff8ac9fde38aa83c43ac7c
OO <opopve@gmail.com>
2019-12-05 21:44:17 +0300

    Console version

--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FilesRenamer
 {
     class MainClass
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(string.Join('\n', args));
+        static void Main(string[] args) {
+
+            DirectoryInfo pathToFiles = new DirectoryInfo(args.FirstOrDefault() ?? "D:\\Temp\\_ren\\1.log");
+            Console.WriteLine(pathToFiles.FullName);
+
+            var path = (pathToFiles.Exists ? pathToFiles : pathToFiles.Parent);
+
+            new Renamer(path, Console.WriteLine).Run();
+
+            Console.WriteLine("Success. Press \"q\" to exit");
             var key = Console.ReadKey();
-            if (key.KeyChar == 'e')
+            if(key.KeyChar == 'q')
                 return;
         }
     }
---
+++ Renamer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FilesRenamer
+{
+    class Renamer
+    {
+        DirectoryInfo path;
+        List<Task> tasks = new List<Task>();
+        Action<string> showMessageAction;
+
+        public Renamer(DirectoryInfo path, Action<string> showMessageAction) {
+            this.path = path;
+            this.showMessageAction = showMessageAction ?? throw new ArgumentNullException(nameof(showMessageAction));
+        }
+
+        public void Run() {
+            foreach(var item in path.GetFileSystemInfos()) {
+                if(item is DirectoryInfo dir) {
+                    var task = new Task(new Renamer(dir, showMessageAction).Run);
+                    tasks.Add(task);
+                    task.Start();
+                    continue;
+                }
+
+                int counter = -1;
+                if(item is FileInfo file) {
+                    var fileWriteTime = file.LastWriteTime.ToString("yyyyMMdd_HHmmssffff");
+                    var fileCreationTime = file.CreationTime.ToString("yyyyMMdd_HHmmssffff");
+                    var sourcePath = file.FullName;
+                    string destinationPath;
+                    do
+                        destinationPath = PrepareDestinationPath(file, fileWriteTime, counter++);
+                    while(new FileInfo(destinationPath).Exists);
+                    File.Move(sourcePath, destinationPath);
+                    counter = -1;
+                }
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        string PrepareDestinationPath(FileInfo file, string newName, int counter = -1) => string.Join(string.Empty, file.DirectoryName, Path.DirectorySeparatorChar, newName, counter < 0 ? string.Empty : "_" + counter, file.Extension);
+    }
+}
